0ebc283a282849319bef231d1ff08bd8b84a0058
Jeremiah H <90330037+JMIAHH@users.noreply.github.com>
2021-11-29 08:55:41 -0500

    v0.0
    Imported pygame
    started pygame
    created game window

---
+++ .github/PyGamePractice.py
@@ -0,0 +1,11 @@
+#PyGame Practice, Jeremiah Harrell, 11/29/2021, 8:42am, v0.0
+
+import pygame, sys
+from pygame.locals import *
+
+#start Pygame
+pygame.init()
+
+# Create game window
+windowSurface = pygame.display.set_mode((500,400), 0, 32)
+pygame.display.set_capiton("Hello, world!")
